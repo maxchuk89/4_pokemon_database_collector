5c527cdc1adab4087e0c3f03aed0d3a44bde35ad
maxchuk89 <maxchuk89@gmail.com>
2025-07-31 22:04:17 +0300

    Настроено отображение моделей в админке.

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from .models import Pokemon, PokemonEntity
 
 
-admin.site.register(Pokemon)
-admin.site.register(PokemonEntity)
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ['title_ru', 'title_en', 'title_jp']
+    search_fields = ['title_ru']
+
+
+@admin.register(PokemonEntity)
+class PokemonEntityAdmin(admin.ModelAdmin):
+    list_display = ['pokemon', 'lat', 'lon', 'appeared_at', 'disappeared_at']
+    list_filter = ['pokemon']
