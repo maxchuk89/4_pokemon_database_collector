c725621f46c70b200c41ede6982dfca8327c0ea1
maxchuk89 <maxchuk89@gmail.com>
2025-07-31 22:02:09 +0300

    Сделал поля моделей необязательными для поддержки неполных данных

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,65 +2,48 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(
-        max_length=200,
-        verbose_name='Имя на русском',
-    )
-    title_en = models.CharField(
-        max_length=200,
-        blank=True,
-        verbose_name='Имя на английском',
-    )
-    title_jp = models.CharField(
-        max_length=200,
-        blank=True,
-        verbose_name='Имя на японском',
-    )
-    photo = models.ImageField(
-        upload_to='pokemons',
-        null=True,
-        verbose_name='Изображение',
-    )
-    description = models.TextField(
-        blank=True,
-        verbose_name='Описание',
-    )
+    title_ru = models.CharField('Название на русском', max_length=200)
+    title_en = models.CharField('Название на английском', max_length=200, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, blank=True)
+    image = models.ImageField('Изображение', upload_to='pokemons', blank=True)
+    description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey(
         'self',
-        on_delete=models.SET_NULL,
+        verbose_name='Предыдущая эволюция',
         null=True,
         blank=True,
-        related_name='next_evolutions',
-        verbose_name='Из кого эволюционировал',
+        on_delete=models.SET_NULL,
+        related_name='next_evolutions'
     )
+
+    def __str__(self):
+        return self.title_ru
 
     class Meta:
         verbose_name = 'Покемон'
         verbose_name_plural = 'Покемоны'
 
-    def __str__(self) -> str:
-        return self.title
-
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(
         Pokemon,
-        on_delete=models.CASCADE,
         verbose_name='Покемон',
+        on_delete=models.CASCADE,
+        related_name='entities'
     )
-    lat = models.FloatField(verbose_name='Широта')
-    lon = models.FloatField(verbose_name='Долгота')
-    appeared_at = models.DateTimeField(verbose_name='Появился в')
-    disappeared_at = models.DateTimeField(verbose_name='Исчез в')
-    level = models.IntegerField(verbose_name='Уровень')
-    health = models.IntegerField(verbose_name='Здоровье')
-    strength = models.IntegerField(verbose_name='Сила')
-    defence = models.IntegerField(verbose_name='Защита')
-    stamina = models.IntegerField(verbose_name='Выносливость')
+    lat = models.FloatField('Широта', blank=True, null=True)
+    lon = models.FloatField('Долгота', blank=True, null=True)
+    appeared_at = models.DateTimeField('Появляется в', blank=True, null=True)
+    disappeared_at = models.DateTimeField('Исчезает в', blank=True, null=True)
+    level = models.IntegerField('Уровень', blank=True, null=True)
+    health = models.IntegerField('Здоровье', blank=True, null=True)
+    strength = models.IntegerField('Сила', blank=True, null=True)
+    defence = models.IntegerField('Защита', blank=True, null=True)
+    stamina = models.IntegerField('Выносливость', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.pokemon} — уровень {self.level or "?"}'
 
     class Meta:
-        verbose_name = 'Сущность покемона'
-        verbose_name_plural = 'Сущности покемонов'
-
-    def __str__(self) -> str:
-        return f'{self.pokemon.title} — {self.level} lvl'
+        verbose_name = 'Появление покемона'
+        verbose_name_plural = 'Появления покемонов'
