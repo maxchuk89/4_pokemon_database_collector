6d56fd3afcc4169425d4f331af9037a9ef9f3460
maxchuk89 <maxchuk89@gmail.com>
2025-07-31 21:36:44 +0300

    Добавлено поле previous_evolution для отображения предыдущей формы покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,13 +4,20 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(null=True, blank=True)
+    previous_evolution = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='next_evolutions',
+        verbose_name='Из кого эволюционировал',
+    )
 
     def __str__(self) -> str:
         return self.title
 
 
 class PokemonEntity(models.Model):
-
     pokemon = models.ForeignKey(
         Pokemon,
         on_delete=models.CASCADE
